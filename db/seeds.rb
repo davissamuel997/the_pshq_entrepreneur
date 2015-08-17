4a5b26ee280085728204143d7c31a1c35aa898d4
SamuelDavis <sam9@cfu.net>
2015-08-17 16:49:34 -0400

    added seeds and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.where(:first_name => 'Samuel', :last_name => 'Davis', :email => 'samuel@problemsolutionhq.com').first_or_create(:password => 'password', :password_confirmation => 'password')
+user2 = User.where(:first_name => 'Peter', :last_name => 'Sperry', :email => 'peter@problemsolutionhq.com').first_or_create(:password => 'password', :password_confirmation => 'password')
