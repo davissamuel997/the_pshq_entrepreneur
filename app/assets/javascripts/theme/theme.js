3692982d25df2ae916c95e6dddfcb54bfe0acb1a
SamuelDavis <sam9@cfu.net>
2015-08-17 11:19:05 -0400

    added in bootstrap js

---
+++ app/assets/javascripts/theme/theme.js
@@ -0,0 +1,13 @@
+/*
+*= require theme/bootstrap.js
+*= require theme/bootstrap-hover-dropdown.js
+*= require theme/bootstrap-switch.min.js
+*= require theme/html5shiv.js
+*= require theme/respond.min.js
+*= require theme/jquery.cookie.js
+*= require theme/jquery.pulsate.js
+*= require theme/jquery-responsive.js
+*= require theme/jquery.validate.js
+*= require theme/jquery.bootstrap.wizard.js
+*= require theme/core.js
+*/
