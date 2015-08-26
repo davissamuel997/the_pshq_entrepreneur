23c57ee1261c4682337619745918466efc41b0c3
SamuelDavis <sam9@cfu.net>
2015-08-26 09:19:06 -0400

    setting up ng bind

--- app/assets/javascripts/angular/app/the_pshq_entrepreneur_module.js
+++ app/assets/javascripts/angular/app/the_pshq_entrepreneur_module.js
@@ -1,1 +1,1 @@
-var ThePshqEntrepreneur = angular.module('ThePshqEntrepreneur', ['ngRoute', 'ngResource', 'rails', 'ng-rails-csrf', 'ui.router', 'ui.bootstrap', 'localytics.directives', 'ngAudio']);
+var ThePshqEntrepreneur = angular.module('ThePshqEntrepreneur', ['ngRoute', 'ngResource', 'rails', 'ng-rails-csrf', 'ui.router', 'ui.bootstrap', 'localytics.directives', 'ngAudio', 'ngSanitize']);
